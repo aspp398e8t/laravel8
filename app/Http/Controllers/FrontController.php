7b82395af635e7471806afbd664acdca9d811a65
aspp398e8t <95395323+aspp398e8t@users.noreply.github.com>
2021-12-14 13:14:46 +0800

    new

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
+use App\Models\News;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,7 +12,7 @@ class FrontController extends Controller
     //
     public function index()
     {
-        return view('index');
+        return view('welcome');
     }
     public function hello()
     {
@@ -22,7 +24,7 @@ class FrontController extends Controller
     }
     public function news()
     {
-        $news = DB::table('news')->get();
+        $news = News::get();
         return view('news-list', compact('news'));
     }
 
@@ -30,5 +32,37 @@ class FrontController extends Controller
     {
         $news = DB::table('news')->find($id);
         return view('news-content', compact('news'));
+    }
+    public function createNews()
+    {
+        News::create([
+            'title' => '攜手众社會企業推動臺灣露營區無障礙環境 交通部觀光局連續三年獲社會創新採購獎肯定',
+            'date' => '2021-12-08',
+            'content' => '臺灣露營夯！越來越多家庭，除了帶小朋友去露營之外，也想帶行動不便的長輩和身心障礙的朋友一起去露營。',
+            'image_url' => 'https://www.taiwan.net.tw/pic.ashx?qp=/0042228/13_0042228_2.jpg&sizetype=2',
+        ]);
+    }
+    public function updateNews($id)
+    {
+        News::find($id)->update([
+            'title' => '55566',
+            'date' => '2021-12-10',
+            'content' => '55',
+            'image_url' => 'https://www.taiwan.net.tw/pic.ashx?qp=/0042228/13_0042228_2.jpg&sizetype=2',
+        ]);
+    }
+    public function deleteNews($id)
+    {
+        News::find($id)->delete([]);
+    }
+    public function contact(Request $request)
+    {
+        Contact::create([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'content' => $request->content,
+        ]);
+        return redirect('/news');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,15 @@ Route::get('/hello', [FrontController::class, 'hello']);
 Route::get('/news', [FrontController::class, 'news']);
 
 Route::get('/news/{id}', [FrontController::class, 'newsContent']);
+
+Route::get('/createNews', [FrontController::class, 'createNews']);
+
+Route::get('/updateNews/{id}', [FrontController::class, 'updateNews']);
+
+Route::get('/deleteNews/{id}', [FrontController::class, 'deleteNews']);
+
+Route::post('/contact', [FrontController::class, 'contact']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
